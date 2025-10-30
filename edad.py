efd0c95042e40ebca8ddd3cc542b70cf7a42213b
Pablo Quiroz <pabloanibaalq@gmail.com>
2025-10-30 12:59:13 +0000

    Day 1: added age calculator script

---
+++ edad.py
@@ -0,0 +1,10 @@
+#Dia 1 - Calculo de edad
+
+nombre = input("¿Como te llamas? ")
+nacimiento = int(input("¿En que año naciste "))
+
+#Calculamos la edad en 2025
+
+edad = 2025 - nacimiento
+
+print(f"Hola {nombre}, naciste en {nacimiento} y tienes {edad} años.")
